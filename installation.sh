552744ae8e6dc16bb909e981a5d33f23c5b21561
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-03-02 16:11:44 +0300

    script d'installation apache et openssh

--- installation.sh
+++ installation.sh
@@ -9,11 +9,13 @@ sudo apt-get install apache2
 #Installer un serveur ssh pour pouvoir accéder à  distance à la machine virtuelle
 sudo apt-get install openssh-server
 
-#Entrer dans le dossier d'apache
-cd /etc/apache2/
+#Connecter la machine réelle avec  la machine virtuelle
+read -p "Saisir le nom d'utilisateur de la machine réelle" hostname
+read -p "Saisir l'ip de la machine réelle" serveur
+ssh $hostname@$serveur
 
 #créer un lien symbolic mettant en relation apache et notre site web
-sudo ln -s ./Desktop/Exam SYS/web/  /var/www/html/
+sudo ln -s ./Desktop/Exam SYS/Web/  /var/www/html/
 
 #Appliquer la nouvelle configuration
 sudo service apache2 reload
